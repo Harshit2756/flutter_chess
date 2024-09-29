d941d395b43df31c2b3aca222ee61e1c37697cc5
Harshit khandelwal <khandelwalharshit431@gmail.com>
2024-09-29 12:04:44 +0530

    removed devicepreview

--- lib/main.dart
+++ lib/main.dart
@@ -5,44 +5,9 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    DevicePreview(
-      backgroundColor: backgroundColor,
-      tools: const [
-        DeviceSection(
-          frameVisibility: false,
-          orientation: false,
-        ),
-        SystemSection(
-          locale: false,
-          theme: true,
-        ),
-        // AccessibilitySection(
-        //   accessibleNavigation: false,
-        //   boldText: false,
-        //   invertColors: false,
-        //   textScalingFactor: false,
-        // ),
-        SettingsSection(),
-      ],
-      devices: [
-        Devices.android.samsungGalaxyA50,
-        Devices.android.samsungGalaxyNote20,
-        Devices.android.samsungGalaxyS20,
-        Devices.android.samsungGalaxyNote20Ultra,
-        Devices.android.onePlus8Pro,
-        Devices.android.sonyXperia1II,
-        Devices.ios.iPhoneSE,
-        Devices.ios.iPhone12,
-        Devices.ios.iPhone12Mini,
-        Devices.ios.iPhone12ProMax,
-        Devices.ios.iPhone13,
-        Devices.ios.iPhone13ProMax,
-        Devices.ios.iPhone13Mini,
-        Devices.ios.iPhoneSE,
-      ],
-      enabled: false,
-      builder: (context) => const MyApp(),
-    ),
+    
+       const MyApp(),
+    
   );
 }
 
@@ -51,12 +16,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Chess',
       debugShowCheckedModeBanner: false,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      home: const GameBoard(),
+      home: GameBoard(),
     );
   }
 }
